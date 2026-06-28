ac969e78dd7f6dfb62160160242649685a5f1a7c
NasserSalim1 <nasser.salim.beserra@gmail.com>
2026-06-28 17:51:58 -0300

    refactor: reorganiza estrutura do ETL e configuração do banco

---
+++ import-ec2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 15:22:51 2026
+
+@author: nasser
+"""
+
---
+++ load.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 10:06:54 2026
+
+@author: nasse
+"""
+
